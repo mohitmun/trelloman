2fd1046f8d6ca65ca442db8e85557d30f2bd3f1a
Mohit M <mohitcrox@gmail.com>
2017-08-04 00:55:23 +0530

    mc

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,11 +8,26 @@ class WelcomeController < ApplicationController
 
   def powerup_index
     headers["X-Frame-Options"] = "ALLOWALL"
+    gon.card_button_text = "Auto Due Date"
+    gon.application_name = User.get_manifest.name
     render "welcome/powerup_index"
   end
 
+  def auto_due
+    if current_user
+      current_user.auto_update_card = params[:enable]
+      current_user.save
+      render json: {auto_due: current_user.auto_update_card}
+    else
+      render json: {message: "User not signed in"}
+    end
+  end
+
   def save_token
-    User.create_user_from_trello_token(params[:token])
+    u = User.create_user_from_trello_token(params[:token])
+    if !u.blank?
+      sign_in u
+    end
     render json: {success: true}
   end
 
@@ -21,6 +36,11 @@ class WelcomeController < ApplicationController
     if request.post?
       User.handle_webhook(params[:welcome])
     end
+    render json: {success: true}
+  end
+
+  def incoming_sutime
+    User.incoming_sutime(params[:welcome])
     render json: {success: true}
   end
 
