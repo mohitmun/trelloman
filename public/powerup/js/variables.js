196a85d95749518d55e19eb1836f55351a86edb5
Mohit M <mohitcrox@gmail.com>
2017-08-03 09:11:22 +0530

    variables js

--- public/powerup/js/variables.js
+++ public/powerup/js/variables.js
@@ -1,55 +1,41 @@
-// before_auth_overlay_options = gon.before_auth_overlay_options.map(function(item) {
-  
-//   return {text: item.text, callback: function(t){
+authorize_opts = {
+  name: "Trelloman",
+  type: "popup",
+  expiration: "never",
+  scope: { read: true, write: true, account: true},
+  success: function() {
+    save_token();
+    console.log("success trelloman")
+  },
+  error: function(a){ 
+    console.log("error trelloman")
+    console.log(a)
+  }
+}
 
-//   }}
-// })
-if (!Trello.authorized()){
-  boardButtons = [
+boardButtons = [
     {
-      text: "Authorize Trello",
-      callback: function(t) {
-        opts = {
-          name: "wow",
-          type: "popup",
-          expiration: "never",
-          success: function() {
-            save_token();
-            console.log("success trelloman")
-          },
-          error: function(){ 
-            console.log("error trelloman")
-          }
-        }
-        Trello.authorize(opts) 
+      text: 'Open Overlay',
+      callback: function(t){
+        return t.overlay({
+          url: './overlay.html',
+          args: { rand: (Math.random() * 100).toFixed(0) }
+        })
+        .then(function(){
+          return t.closePopup();
+        });
+      }
+    },
+    {
+      text: 'Open Board Bar',
+      callback: function(t){
+        return t.boardBar({
+          url: './board-bar.html',
+          height: 200
+        })
+        .then(function(){
+          return t.closePopup();
+        });
       }
     }
   ]
-}else{
-  boardButtons = [
-      {
-        text: 'Open Overlay',
-        callback: function(t){
-          return t.overlay({
-            url: './overlay.html',
-            args: { rand: (Math.random() * 100).toFixed(0) }
-          })
-          .then(function(){
-            return t.closePopup();
-          });
-        }
-      },
-      {
-        text: 'Open Board Bar',
-        callback: function(t){
-          return t.boardBar({
-            url: './board-bar.html',
-            height: 200
-          })
-          .then(function(){
-            return t.closePopup();
-          });
-        }
-      }
-    ]
-}
