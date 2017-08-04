8e30a79a5483398a080e3bf1f654faa3104a2ff7
Mohit M <mohitcrox@gmail.com>
2017-08-04 09:10:26 +0530

    mc

--- public/powerup/js/variables.js
+++ public/powerup/js/variables.js
@@ -7,35 +7,67 @@ authorize_opts = {
     save_token();
     console.log("success trelloman")
   },
-  error: function(a){ 
+  error: function(){ 
     console.log("error trelloman")
-    console.log(a)
+    localStorage.clear()
   }
 }
 
-boardButtons = [
+boardButtons = function() {
+  result_if_not_enabled = [
     {
-      text: 'Open Overlay',
+      text: 'Enable Auto Due Date',
       callback: function(t){
-        return t.overlay({
-          url: './overlay.html',
-          args: { rand: (Math.random() * 100).toFixed(0) }
-        })
-        .then(function(){
-          return t.closePopup();
+        enable = true
+        localStorage.setItem("auto_due", enable)
+        $.getJSON("/auto_due?enable="+ enable, function(data){
+          console.log("auto_due ")
+          console.log(data)
         });
+        t.closePopup()
+        // return t.overlay({
+        //   url: './overlay.html',
+        //   args: { rand: (Math.random() * 100).toFixed(0) }
+        // })
+        // .then(function(){
+        //   return t.closePopup();
+        // });
       }
-    },
+    }
+    // ,
+    // {
+    //   text: 'Open Board Bar',
+    //   callback: function(t){
+    //     return t.boardBar({
+    //       url: './board-bar.html',
+    //       height: 200
+    //     })
+    //     .then(function(){
+    //       return t.closePopup();
+    //     });
+    //   }
+    // }
+  ]
+  result_if_enabled = [
     {
-      text: 'Open Board Bar',
+      text: 'Disable Auto Due Date',
       callback: function(t){
-        return t.boardBar({
-          url: './board-bar.html',
-          height: 200
-        })
-        .then(function(){
-          return t.closePopup();
-        });
+        enable = false
+        localStorage.setItem("auto_due", enable)
+        $.getJSON("/auto_due?enable="+ enable, function(data){
+          console.log("auto_due ")
+          console.log(data)
+        }); 
+        t.closePopup()
       }
     }
   ]
+  auto_due = localStorage.getItem("auto_due")
+  console.log("auto_due:" + auto_due)
+  if (auto_due == "true"){
+    return result_if_enabled
+  }else{
+    return result_if_not_enabled
+  }
+
+}
